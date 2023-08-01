70a92f32f9b227e9731ac0e09f7e8fe43790c395
Mina <wakmanmeena07@gmail.com>
2023-08-01 11:49:25 +0100

    Initial Commit

---
+++ Climiz/ClimizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimizApp.swift
+//  Climiz
+//
+//  Created by scholar on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClimizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
